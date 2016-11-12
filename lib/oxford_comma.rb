f7db6648cebc28c4cad20a3b288d2682df3e30d2
Kevin Wang <kev.wang424@gmail.com>
2016-11-12 00:47:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  answer = ""
+  if array.length == 1
+    return array.join(",")
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    (0...array.length - 1).each {|idx| answer << "#{array[idx]}, "}
+    answer << "and #{array[-1]}"
+  end
+  answer
 end
